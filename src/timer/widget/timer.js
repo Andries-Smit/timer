dc0992d1752e09b68ef89e6f25dea1f97cae8e80
Andries Smit <mr.andries.smit@gmail.com>
2017-05-09 13:37:05 +0200

    Implmenet clock and timer

--- src/timer/widget/timer.js
+++ src/timer/widget/timer.js
@@ -21,10 +21,17 @@ define([
 
     return declare("timer.widget.timer", [ _WidgetBase ], {
 
+        // Modeler attribtues
+        formatTime: "",
+        type: "clock", // "clock" | "duration"
+        refreshInterval: 1,
+        startTimeAttribute: "",
 
         // Internal variables.
         _handles: null,
         _contextObj: null,
+        _intervalHandle: null,
+        _startTime: null,
 
         constructor: function () {
             this._handles = [];
@@ -32,30 +39,69 @@ define([
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
+            if (this.type === "clock") {
+                this.startClock();
+            }
         },
 
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
+
             this._updateRendering(callback);
         },
 
+        updateClock: function() {
+            var time;
+            if(this.type === "clock") {
+                time = new Date();
+            } else {
+                time = new Date() - this._startTime;
+            }
+            this.domNode.innerHTML = mx.parser.formatValue(time, "datetime", {
+                datePattern: this.formatTime
+            });
+        },
+
+        startClock: function() {
+            dojoStyle.set(this.domNode, "display", "block");
+            if (!this._intervalHandle) {
+                this._intervalHandle = window.setInterval(lang.hitch(this, this.updateClock), this.refreshInterval);
+            }
+        },
+
+        stopClock: function(){
+            dojoStyle.set(this.domNode, "display", "none");
+            if(this._intervalHandle) {
+                window.clearInterval(this._intervalHandle);
+                this._intervalHandle = null;
+            }
+        },
+
         resize: function (box) {
-          logger.debug(this.id + ".resize");
+            logger.debug(this.id + ".resize");
         },
 
         uninitialize: function () {
-          logger.debug(this.id + ".uninitialize");
+            logger.debug(this.id + ".uninitialize");
+            this.stopClock();
         },
 
         _updateRendering: function (callback) {
             logger.debug(this.id + "._updateRendering");
 
             if (this._contextObj !== null) {
-                dojoStyle.set(this.domNode, "display", "block");
+                if(this.startTimeAttribute && this.type === "duration") {
+                    this._startTime = this._contextObj.get(this.startTimeAttribute);
+                    if(this._startTime) {                        
+                        this.startClock();
+                    } else {
+                        this.stopClock();
+                    }
+                }
             } else {
-                dojoStyle.set(this.domNode, "display", "none");
+                this.stopClock();
             }
 
             this._executeCallback(callback, "_updateRendering");
